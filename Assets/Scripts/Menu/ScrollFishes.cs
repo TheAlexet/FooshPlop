f8bdd58842e5e6da97a44c6026ad9e9e52030cc8
Raffaelbdl <raffaelbdl@gmail.com>
2022-11-24 14:07:42 +0900

    🔨 Refactor scrollsystem

--- Assets/Scripts/Menu/ScrollFishes.cs
+++ Assets/Scripts/Menu/ScrollFishes.cs
@@ -1,61 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-
-public class ScrollFishes : MonoBehaviour
+public class ScrollFishes : ScrollSystem
 {
-    [SerializeField] GameObject contentHolder;
-    [SerializeField] ScrollRect scrollRect;
-    [SerializeField] TMPro.TextMeshProUGUI levelText;
-    public float looseStep = 0.1f;
-    public float reducedSize = 0.6f;
-
-    [Header("Debug Values")]
-    [SerializeField] int closestFish = 0;
-    [SerializeField] float scrollValue = 0f;
-    [SerializeField] float[] positions;
-    [SerializeField] Vector3 normalScale;
-
-    void Start()
+    public override string GetText()
     {
-        positions = new float[contentHolder.transform.childCount];
-        for (int i = 0; i < contentHolder.transform.childCount; i++)
-        {
-            positions[i] = (float)i / (contentHolder.transform.childCount - 1);
-        }
-
-        normalScale = contentHolder.transform.GetChild(0).GetChild(0).localScale;
+        return contentHolder.transform.GetChild(closestPosition).GetComponentInChildren<FishData>().fancyName;
     }
-
-
-    void Update()
-    {
-        closestFish = GetClosestFish();
-        UpdateFishText();
-    }
-
-    int GetClosestFish()
-    {
-        float curPos = scrollRect.horizontalNormalizedPosition;
-        float halfDistance = 0.5f / (contentHolder.transform.childCount - 1);
-
-        for (int i = 0; i < contentHolder.transform.childCount; i++)
-        {
-            float childPos = positions[i];
-            if (curPos - halfDistance < childPos && childPos <= curPos + halfDistance)
-            {
-                return i;
-            }
-        }
-        return 0;
-    }
-
-
-    void UpdateFishText()
-    {
-        levelText.text = contentHolder.transform.GetChild(closestFish).GetComponentInChildren<FishData>().fancyName;
-    }
-
 }
