dc9048c0930b36823691a4bb927e45fbd90835f0
Raffaelbdl <raffaelbdl@gmail.com>
2022-12-07 10:23:12 +0900

    ⚡ Quick add Leave Button

--- Assets/Scripts/Menu/SceneChange.cs
+++ Assets/Scripts/Menu/SceneChange.cs
@@ -3,23 +3,25 @@ using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    [SerializeField] Database db;
-    [SerializeField] TMPro.TextMeshProUGUI acornsText;
+    // [SerializeField] Database db;
+    // [SerializeField] TMPro.TextMeshProUGUI acornsText;
 
-    public void ChangeScene(int sceneID)
-    {
-        int level = sceneID - 1;
-        int acorns = (level - 1) * 100;
+    // public void ChangeScene(int sceneID)
+    // {
+    //     int level = sceneID - 1;
+    //     int acorns = (level - 1) * 100;
 
-        if(db.getMaxLevel() >= level)
-        {
-            SceneManager.LoadScene(sceneID);
-        }
-        else if(db.getAcorns() >= acorns && level < 3)
-        {
-            db.setMaxLevel(db.getMaxLevel() + 1);
-            db.setAcorns(db.getAcorns() - acorns);
-            acornsText.text = db.getAcorns().ToString();
-        }
-    }
+    //     if(db.getMaxLevel() >= level)
+    //     {
+    //         SceneManager.LoadScene(sceneID);
+    //     }
+    //     else if(db.getAcorns() >= acorns && level < 3)
+    //     {
+    //         db.setMaxLevel(db.getMaxLevel() + 1);
+    //         db.setAcorns(db.getAcorns() - acorns);
+    //         acornsText.text = db.getAcorns().ToString();
+    //     }
+    // }
+
+    public void ChangeScene(int sceneID) { SceneManager.LoadScene(sceneID); }
 }
