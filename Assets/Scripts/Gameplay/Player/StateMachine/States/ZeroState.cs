0cb3827ae657578af7487722942b3022d9f4b607
TheAlexet <thealexet6@gmail.com>
2023-01-13 09:42:49 +0900

    Tutorial menu
    Tutorial menu appears after 10 sec of no interaction in every level

--- Assets/Scripts/Gameplay/Player/StateMachine/States/ZeroState.cs
+++ Assets/Scripts/Gameplay/Player/StateMachine/States/ZeroState.cs
@@ -6,15 +6,24 @@ public class ZeroState : InteractiveState
 {
     public ZeroState(FishingSM stateMachine) : base("Zero", stateMachine) { }
 
+    float timer = 0.0f;
+
     public override void UpdateLogic()
     {
         base.UpdateLogic();
         // . transition to "Cast" state if input < -3
         if (_gyroRotationRate < -3f)
         {
+            _sm.tutorialMenu.SetActive(false);
             _sm.throwSound.Play();
             _sm.playerAnimator.SetTrigger("Cast");
             stateMachine.ChangeState(_sm.castState);
+        }
+
+        timer += Time.deltaTime;
+        if(timer >= 10)
+        {
+            _sm.tutorialMenu.SetActive(true);
         }
     }
 
